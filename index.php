210935f909848d6cfa9990b0f756cbea55930f64
Michael Highland <michael@iamfutureproof.com>
2012-01-14 17:35:57 -0500

    integrated compass!

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="utf-8">
 		<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-		<script src="jquery-1.7.1.min.js" type="text/javascript"></script>
-		<script src="jquery.ui.core.js" type="text/javascript"></script>
-		<script src="site.js" type="text/javascript"></script>
+		<script src="javascripts/jquery-1.7.1.min.js" type="text/javascript"></script>
+		<script src="javascripts/jquery.ui.core.js" type="text/javascript"></script>
+		<script src="javascripts/site.js" type="text/javascript"></script>
 		<title>Intention</title>
-		<link href="style.css" media="screen" rel="stylesheet" type="text/css" /> 
+		<link href="css/style.css" media="screen" rel="stylesheet" type="text/css" /> 
 		<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
