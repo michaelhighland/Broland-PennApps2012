8db2f055343f7fcc11f1a0579b26bd8e5b3c24f6
Michael Highland <michael@iamfutureproof.com>
2012-01-14 00:02:03 -0500

    basic jquery

--- site.js
+++ site.js
@@ -1,4 +1,20 @@
 (function() {
-  var test;
-  test = 1;
+  var randomInRange;
+  randomInRange = function(x, y) {
+    return Math.floor(Math.random() * x) + y;
+  };
+  window.Application = (function() {
+    var GLOBAL_VAR;
+    GLOBAL_VAR = 1000;
+    function Application() {
+      this.init();
+    }
+    Application.prototype.init = function() {
+      return alert("hello world again");
+    };
+    return Application;
+  })();
+  $(function() {
+    return new Application;
+  });
 }).call(this);
